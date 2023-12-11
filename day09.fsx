9f415f663a3832b38f78ec10bc63f224775adb7c
Martin Björkström <martin@bjorkstrom.dev>
2023-12-11 23:03:43 +0200

    Day 9

---
+++ day09.fsx
@@ -0,0 +1,37 @@
+
+let diffs nums =
+    nums
+    |> List.pairwise
+    |> List.map (fun (a, b) -> b - a)
+
+let extrapolate nums =
+    let rec loop state nums =
+        let nums' = nums |> diffs
+        let state = state + nums[^0]
+        if nums' |> List.pairwise |> List.forall (fun (a,b) -> a = b) then
+            state + nums'[^0]
+        else
+            nums' |> loop state
+    nums |> loop 0
+
+let parse filename =
+    filename
+    |> System.IO.File.ReadAllLines
+    |> Array.map (fun line ->
+        line.Split ' '
+        |> Array.map int
+        |> Array.toList)
+    |> Array.toList
+
+let part1 filename = 
+    filename
+    |> parse
+    |> List.map extrapolate
+    |> List.sum
+
+// Part 2
+let part2 filename = 
+    filename
+    |> parse
+    |> List.map (List.rev >> extrapolate)
+    |> List.sum
